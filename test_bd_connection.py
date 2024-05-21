5035065dc705529f4eac18670d402697ae8c5d5c
pwdrc <tolvo.pedro@gmail.com>
2024-05-21 17:01:50 -0300

    test bd connection

--- test_bd_connection.py
+++ test_bd_connection.py
@@ -1,8 +1,8 @@
 import getpass
 import oracledb
 
-user = 'a10492012'
-password = 'a10492012'
+user = ''
+password = ''
 dsn = 'orclgrad1.icmc.usp.br:1521/pdb_elaine.icmc.usp.br'
 
 with oracledb.connect(user=user, password=password, dsn=dsn) as connection:
